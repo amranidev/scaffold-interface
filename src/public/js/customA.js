f3c3ae5056025e275f5ad661f28e47e78b7d4444
Amrani <amranidev@gmail.com>
2015-12-07 16:21:14 +0000

    fix conflicts

---
+++ src/public/js/customA.js
@@ -0,0 +1,17 @@
+$(document).on("click", ".viewEdit", function() {
+	GETT($(this).data('link'));
+})
+$(document).on("click", ".viewShow", function() {
+	GETT($(this).data('link'));
+})
+
+function GETT(dataLink) {
+    $.ajax({
+        async: true,
+        method: 'get',
+        url: baseURL + dataLink,
+        success: function(response) {
+            window.location.replace(response);
+        }
+    });
+}
