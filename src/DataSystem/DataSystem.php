c1acf4c8c899d5b0444182c8dab0974ddcf76c0b
Amrani <amranidev@gmail.com>
2015-12-24 13:55:07 +0000

    selected relations and dataspec error

--- src/DataSystem/DataSystem.php
+++ src/DataSystem/DataSystem.php
@@ -100,16 +100,25 @@ class DataSystem
      */
     private function Tables($data)
     {
-
+        $tmp = '';
         $this->onData = [];
         $this->foreignKeys = [];
         $i = 0;
         $j = 0;
         foreach ($data as $key => $value) {
+
             if ($key == 'tbl' . $i) {
+                $tmp = $value;
+                if (in_array($value, $this->foreignKeys)) {
+                    throw new \Exception($value . " Relation Already selected");
+                }
                 array_push($this->foreignKeys, $value);
                 $i++;
+
             } elseif ($key == 'on' . $j) {
+                if (!in_array($value, Schema::getColumnListing($tmp))) {
+                    throw new \Exception($value . " Does not exist in " . $tmp);
+                }
                 array_push($this->onData, $value);
                 $j++;
             }
