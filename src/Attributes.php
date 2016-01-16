c5109d475902acdaf9ea48f28ba6f729d02a9758
Amrani <amranidev@gmail.com>
2016-01-16 17:53:39 +0000

    Create Attributes

---
+++ src/Attributes.php
@@ -0,0 +1,47 @@
+<?php
+namespace Amranidev\ScaffoldInterface;
+
+use Illuminate\Support\Facades\DB;
+
+/**
+ * class Attributes
+ *
+ * @package ajaxis\Parser
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
+class Attributes
+{
+    /**
+     * table name
+     *
+     * @var $table String
+     */
+    private $table;
+    /**
+     * Result
+     *
+     * @var $Result[]
+     */
+    public $result = [];
+    /**
+     * create new Attrebutes
+     *
+     * @param $table
+     */
+    public function __construct($table)
+    {
+        $this->table = $table;
+    }
+    /**
+     * Get attributes from table
+     */
+    public function getAttributes()
+    {
+        //select all the Attributes from table
+        $this->result = DB::select(DB::raw('show columns from `' . $this->table . '`;'));
+        //delete the first element.(ignore the id section)
+        unset($this->result[0]);
+        //get result
+        return $this->result;
+    }
+}
