c6009cf7e25c40ff6dfd3a34b1748db68935ed9a
Amrani <amranidev@gmail.com>
2016-01-16 17:53:59 +0000

    Create AutoArray.php

---
+++ src/AutoArray.php
@@ -0,0 +1,104 @@
+<?php
+namespace Amranidev\ScaffoldInterface;
+
+use Amranidev\ScaffoldInterface\Attributes;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * class AutoArray (Test v3.0.x-dev)
+ *
+ * @package ajaxis\Autoarray
+ * @author Amrani Houssain <amranidev@gmail.com>
+ *
+ * @todo test if its more practical
+ */
+class AutoArray
+{
+    /**
+     * Result Array
+     *
+     * @var $result
+     */
+    public $result = [];
+    /**
+     * Get Table Attribute (an Attributes Instance)
+     *
+     * @var $attributes
+     */
+    private $attributes;
+    /**
+     * create new AutoArray instance
+     *
+     * @var $table String
+     */
+    public function __construct($table)
+    {
+        $this->attributes = new Attributes($table);
+    }
+    /**
+     * arrayAnalyser . add specified types from attributes
+     *
+     * @var $input Array
+     *
+     * @return $result
+     */
+    public function arrayAnalyzer($input)
+    {
+        $result = [];
+        foreach ($input as $key => $value) {
+            if ($key == "Field") {
+                $result[] = $value;
+            }
+        }
+
+        return $result;
+    }
+    /**
+     * get result from attributes and arrayAnalyzer
+     *
+     * @return $result
+     */
+    public function getResult()
+    {
+        $result = [];
+        foreach ($this->attributes->getAttributes() as $key) {
+            $result[] = $this->arrayAnalyzer($key);
+        }
+        return $result;
+    }
+    /**
+     * merge : get the final result and inject (name,type,key,value)
+     *
+     * @return $this->result
+     */
+    public function merge()
+    {
+        $array = $this->getResult();
+        foreach ($array as $key => $value) {
+            $value['name'] = $value['0'];
+            $value['type'] = $value['1'];
+            $value['key'] = $value['0'] . ' :';
+            $value['value'] = '';
+            unset($value['1']);
+            unset($value['0']);
+            $this->result[] = $value;
+        }
+        return $this->result;
+    }
+    /**
+     * get simple array from Model
+     *
+     * @param Model $model
+     *
+     * @return $result
+     */
+    public function getModelArray(Model $model)
+    {
+        $result = [];
+        foreach ($model->getAttributes() as $key => $value) {
+            $result[] = ['type' => 'text', 'name' => $key, 'key' => $key . ' :', 'value' => $value];
+        }
+        unset($result[0]);
+        return $result;
+    }
+}
