d0872bbca4efc688c6bd4488f06aa2b62348e3a2
Amrani <amranidev@gmail.com>
2016-01-16 17:53:04 +0000

    get attributes route

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -9,3 +9,4 @@ Route::get('scaffold', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiControl
 Route::post('scaffold/guipost', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@store');
 Route::get('scaffold/guirollback/{id}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@destroy');
 Route::get('scaffold/guidelete/{id}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@deleteMsg');
+Route::get('scaffold/getAttributes/{table}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@GetResult');
