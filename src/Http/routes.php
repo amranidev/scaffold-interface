389b6be72833bc51613fd9a637d303dbf3fe4e19
Amrani <amranidev@gmail.com>
2016-02-04 14:18:50 +0000

    add route group for Laravel [5.2]

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,18 +5,22 @@
 |--------------------------------------------------------------------------
 |
  */
-Route::get('scaffold', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@index');
+Route::group(['middleware' => 'web'], function () {
 
-Route::post('scaffold/guipost', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@store');
+    Route::get('scaffold', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@index');
 
-Route::get('scaffold/guirollback/{id}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@destroy');
+    Route::post('scaffold/guipost', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@store');
 
-Route::get('scaffold/guidelete/{id}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@deleteMsg');
+    Route::get('scaffold/guirollback/{id}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@destroy');
 
-Route::get('scaffold/getAttributes/{table}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@GetResult');
+    Route::get('scaffold/guidelete/{id}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@deleteMsg');
 
-Route::get('scaffold/homePage', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@homePage');
+    Route::get('scaffold/getAttributes/{table}', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@GetResult');
 
-Route::get('scaffold/HomePageScaffold', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@getIndex');
+    Route::get('scaffold/homePage', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@homePage');
 
-Route::get('scaffold/HomePageDelete', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@homePageDelete');
+    Route::get('scaffold/HomePageScaffold', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@getIndex');
+
+    Route::get('scaffold/HomePageDelete', '\Amranidev\ScaffoldInterface\Http\Controllers\GuiController@homePageDelete');
+
+});
