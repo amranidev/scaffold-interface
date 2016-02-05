d7514980a186b9b5acc11fbc72468761ce144108
Amrani <amranidev@gmail.com>
2016-02-05 23:35:31 +0000

    redirects to the new location customA

--- public/js/customA.js
+++ public/js/customA.js
@@ -10,7 +10,7 @@ function GETT(dataLink) {
         method: 'get',
         url: baseURL + dataLink,
         success: function(response) {
-            window.location.replace(response);
+            window.location = response
         }
     });
 }
