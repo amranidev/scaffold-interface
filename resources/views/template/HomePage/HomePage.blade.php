e1061b7a1301b831576e4ab179da0733664c6e63
Amrani <amranidev@gmail.com>
2016-02-07 16:23:09 +0000

    footer change dynamically

--- resources/views/template/HomePage/HomePage.blade.php
+++ resources/views/template/HomePage/HomePage.blade.php
@@ -49,7 +49,7 @@
 			</div>
 			<div class="footer-copyright">
 				<div class="container">
-					© 2014 Copyright Text
+					© {{date('Y')}} Copyright Text
 					<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
 				</div>
 			</div>
