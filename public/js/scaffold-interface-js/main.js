144ac833cac9147aaaaf348e75a2a312844b8f72
Amrani <amranidev@gmail.com>
2016-02-26 22:52:58 +0000

    update script test

--- public/js/scaffold-interface-js/main.js
+++ public/js/scaffold-interface-js/main.js
@@ -1,21 +1,40 @@
 new Vue({
+
     el: 'body',
+    
     data: {
+
         show: false,
+        
         submit: false,
+        
         error: false,
-        rows: 0,
+        
         finals: false,
-        select: ['String' , 'date','longText','integer','biginteger','boolean','float'],
+        
+        select: ['String', 'date', 'longText', 'integer', 'biginteger', 'boolean', 'float'],
+        
+        selected: '0',
+        
         baseUrl: baseUrl,
+        
         OneToMany: scaffoldList,
-        OneToManyRows:0,
+        
+        attributes:{},
+        
+        OneToManyRows: 0,
+        
+        rows: 0,
+    
     },
+    
     methods: {
+    
         increment: function() {
             this.error = false;
             this.rows += 1;
         },
+    
         decrement: function() {
             if (this.rows == 0) {
                 this.error = true;
@@ -23,14 +42,27 @@ new Vue({
                 this.rows -= 1;
             }
         },
-        addOneToMany:function()
-        {
+    
+        addOneToMany: function() {
             this.OneToManyRows += 1;
         },
-
-        deleteOneToMany:function()
-        {
+    
+        deleteOneToMany: function() {
             this.OneToManyRows -= 1;
+        },
+
+        getAttr:function()
+        {
+            console.log(this.baseUrl + '/scaffold/getAttributes/' + this.selected);
+            $.ajax({
+                method: 'get',
+                url: this.baseUrl + '/scaffold/getAttributes/' + this.selected,
+                success:function(response){
+                    this.attributes.push(response)
+                    console.log(this.attributes)
+                }
+
+            })
         }
     }
 })
