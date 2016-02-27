cfb489217385b9c3f6f3888ae8d26bbce7b1dccd
Amrani <amranidev@gmail.com>
2016-02-27 21:20:06 +0000

    update script test

--- public/js/scaffold-interface-js/main.js
+++ public/js/scaffold-interface-js/main.js
@@ -1,40 +1,23 @@
 new Vue({
-
     el: 'body',
-    
     data: {
-
         show: false,
-        
         submit: false,
-        
         error: false,
-        
         finals: false,
-        
         select: ['String', 'date', 'longText', 'integer', 'biginteger', 'boolean', 'float'],
-        
         selected: '0',
-        
         baseUrl: baseUrl,
-        
         OneToMany: scaffoldList,
-        
-        attributes:{},
-        
+        attributes: {},
         OneToManyRows: 0,
-        
         rows: 0,
-    
     },
-    
     methods: {
-    
         increment: function() {
             this.error = false;
             this.rows += 1;
         },
-    
         decrement: function() {
             if (this.rows == 0) {
                 this.error = true;
@@ -42,26 +25,25 @@ new Vue({
                 this.rows -= 1;
             }
         },
-    
         addOneToMany: function() {
             this.OneToManyRows += 1;
         },
-    
         deleteOneToMany: function() {
             this.OneToManyRows -= 1;
         },
-
-        getAttr:function()
-        {
+        getAttr: function() {
+            var E = this.OneToManyRows;
+            E -= 1;
+            console.log('#tbl' + E);
+            this.selected = $('#tbl' + E).val();
             console.log(this.baseUrl + '/scaffold/getAttributes/' + this.selected);
             $.ajax({
                 method: 'get',
                 url: this.baseUrl + '/scaffold/getAttributes/' + this.selected,
-                success:function(response){
-                    this.attributes.push(response)
+                success: function(response) {
+                    this.attributes = response
                     console.log(this.attributes)
                 }
-
             })
         }
     }
