36fdcc330cb7a97f23901f776559a9e944700457
Amrani <amranidev@gmail.com>
2016-03-08 22:27:06 +0000

    update view test

--- resources/views/viewTest.blade.php
+++ resources/views/viewTest.blade.php
@@ -2,9 +2,10 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<title>Vue Test</title>
 	</head>
 	<body>
@@ -12,8 +13,9 @@
 			<h2 class = "thin">The <i>Scaffold Interface</i> for laravel</h2>
 			<div style = 'margin-top: 2cm;'></div>
 			<button v-if = '!show' class = 'btn' @click = 'show = ! show'><i class = 'material-icons left'>create</i>NEW</button>
-			<div class="row" v-if = 'show'>
-				<div class="col s5">
+			<br>
+			<div class="row">
+				<div class="col s5" v-if = 'show'>
 					<p class = 'red-text' v-if = 'error'>@{{errorMsg}}</p>
 					<form id = 'form' method = 'post' action = '{{URL::to("/")}}/scaffold/guipost/'>
 						<input type = 'hidden' name = '_token' value = '{{Session::token()}}'>
@@ -56,16 +58,16 @@
 							<tr v-for = "final in OneToManyData">
 								<td>
 									<div class="input-field col s12">
-										<input disable name = "tbl@{{final.id}}" type = "text" required='' aria-required='true' value = "@{{final.table}}">
+										<input disabled name = "tbl@{{final.id}}" type = "text" required='' aria-required='true' value = "@{{final.table}}">
 									</div>
 								</td>
 								<td>
 									<div class="input-field col s12">
-										<input disable name = "on@{{final.id}}" type = "text" required='' aria-required='true' value = "@{{final.onData}}">
+										<input disabled name = "on@{{final.id}}" type = "text" required='' aria-required='true' value = "@{{final.onData}}">
 									</div>
 								</td>
 								<td>
-									<a href="#" class = 'btn-floating red' @click = 'removeRelation(final)'><i class = 'material-icons left'>delete</i></a>
+									<a class = 'btn-floating red' @click = 'removeRelation(final)'><i class = 'material-icons left'>delete</i></a>
 								</td>
 							</tr>
 							<tr v-if = "OneToManyBool">
@@ -79,7 +81,7 @@
 								</td>
 								<td>
 									<select class = 'browser-default' id = "on" name = "on" data-id = "on" @change = 'getOnData($index)'>
-										<option value="" disabled selected>Choose your option</option>
+										<option value="scfld#01" disabled selected>Choose your option</option>
 										<option v-for = "elementt in attributes" value = "@{{ elementt }}">@{{* elementt }}</option>
 										<label for = "on">Select Type</label>
 									</select>
@@ -95,58 +97,62 @@
 					</form>
 				</div>
 				<div class="col s7">
-					<div class='card-panel #fafafa grey lighten-5'>
-						<h4 class = 'center thin'>Rows</h4>
-						<div class = 'row center actionRow'>
-							<a class = 'btn blue' v-if = '!more' @click = "increment"><i class = 'material-icons left'>add</i>new</a>
-							<a v-if = '!more' class = 'btn red' @click = 'decrement'><i class = 'material-icons left'>delete</i>remove</a>
-							<a class = 'btn green' v-if = "!more" @click = 'more = true'><i class = 'material-icons left'>arrow_forward</i>more</a>
-							<a class = 'btn purple' v-if = 'more' @click = 'lastStep'><i class = 'material-icons left'>arrow_back</i>back</a>
-							<a  v-if = 'more && !submit' @click = 'addOneToMany' class = 'btn #0d47a1 blue darken-4'><i class = 'material-icons left'>device_hub</i>One To Many</a>
-							<a  v-if = 'more && !submit' @click = 'lastOne' class = 'btn orange pushlDown'><i class = 'material-icons left'>layers</i>ready</a>
+					<div v-if = 'show'>
+						<div class='card-panel #fafafa grey lighten-5'>
+							<h4 class = 'center thin'>Rows</h4>
+							<div class = 'row center actionRow'>
+								<a class = 'btn blue' v-if = '!more' @click = "increment"><i class = 'material-icons left'>add</i>new</a>
+								<a v-if = '!more' class = 'btn red' @click = 'decrement'><i class = 'material-icons left'>delete</i>remove</a>
+								<a class = 'btn green' v-if = "!more" @click = 'more = true'><i class = 'material-icons left'>arrow_forward</i>more</a>
+								<a class = 'btn purple' v-if = 'more' @click = 'lastStep'><i class = 'material-icons left'>arrow_back</i>back</a>
+								<a  v-if = 'more && !submit' @click = 'addOneToMany' class = 'btn #0d47a1 blue darken-4'><i class = 'material-icons left'>device_hub</i>One To Many</a>
+								<a  v-if = 'more && !submit' @click = 'lastOne' class = 'btn orange pushlDown'><i class = 'material-icons left'>layers</i>ready</a>
+							</div>
 						</div>
 					</div>
 					<!-- Interface Core-->
-					<table class = 'centered highlight'>
-						<thead>
-							<th>Name</th>
-							<th>Created at</th>
-							<th>State</th>
-							<th>Link</th>
-							<th>Rollback</th>
-						</thead>
-						<tbody>
-							@foreach($scaffold as $value)
-							<tr>
-								<td>{{$value->tablename}}</td>
-								<td>{{$value->created_at}}</td>
-								<td><span class = "scaffoldv {{$toto = Schema::hasTable($value->tablename) ? 'green' : 'red'}} white-text">{{$toto = Schema::hasTable($value->tablename) ? 'Migrated' : 'Not migrated'}}</span></td>
-								<td><a href="{{URL::to('/')}}/{{lcfirst(str_singular($value->tablename))}}" class = 'btn-floating blue white-text'><i class = 'material-icons'>send</i></a></td>
-								<td><a href = '#modal1' class = 'delete btn-floating modal-trigger pink' data-link = '/scaffold/guidelete/{{$value->id}}/'><i class = 'material-icons'>repeat</i></a></td>
-							</tr>
-							@endforeach
-						</tbody>
-					</table>
-					{!! $scaffold->render() !!}
-					<div class="pushDown"></div>
-					<span>Scaffold-interface <span class = 'scaffoldv orange white-text'>dev-master</span></span>
-					<p class = 'light'>Copyright (c) {{date('Y')}} Amrani Houssian<br><br>
-						Permission is hereby granted, free of charge, to any person obtaining a copy
-						of this software and associated documentation files (the "Scaffold-Interface"), to deal
-						in the Software without restriction, including without limitation the rights
-						to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-						copies of the Software, and to permit persons to whom the Software is
-						furnished to do so, subject to the following conditions:<br><br>
-						The above copyright notice and this permission notice shall be included in
-						all copies or substantial portions of the Software.<br><br>
-						THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-						IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-						FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-						AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-						LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-						OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-						THE SOFTWARE.
-					</p>
+					<div>
+						<table class = 'centered highlight'>
+							<thead>
+								<th>Name</th>
+								<th>Created at</th>
+								<th>State</th>
+								<th>Link</th>
+								<th>Rollback</th>
+							</thead>
+							<tbody>
+								@foreach($scaffold as $value)
+								<tr>
+									<td>{{$value->tablename}}</td>
+									<td>{{$value->created_at}}</td>
+									<td><span class = "scaffoldv {{$toto = Schema::hasTable($value->tablename) ? 'green' : 'red'}} white-text">{{$toto = Schema::hasTable($value->tablename) ? 'Migrated' : 'Not migrated'}}</span></td>
+									<td><a href="{{URL::to('/')}}/{{lcfirst(str_singular($value->tablename))}}" class = 'btn-floating blue white-text'><i class = 'material-icons'>send</i></a></td>
+									<td><a href = '#modal1' class = 'delete btn-floating modal-trigger pink' data-link = '/scaffold/guidelete/{{$value->id}}/'><i class = 'material-icons'>repeat</i></a></td>
+								</tr>
+								@endforeach
+							</tbody>
+						</table>
+						{!! $scaffold->render() !!}
+						<div class="pushDown"></div>
+						<span>Scaffold-interface <span class = 'scaffoldv orange white-text'>dev-master</span></span>
+						<p class = 'light'>Copyright (c) {{date('Y')}} Amrani Houssian<br><br>
+							Permission is hereby granted, free of charge, to any person obtaining a copy
+							of this software and associated documentation files (the "Scaffold-Interface"), to deal
+							in the Software without restriction, including without limitation the rights
+							to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+							copies of the Software, and to permit persons to whom the Software is
+							furnished to do so, subject to the following conditions:<br><br>
+							The above copyright notice and this permission notice shall be included in
+							all copies or substantial portions of the Software.<br><br>
+							THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+							IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+							FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+							AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+							LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+							OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+							THE SOFTWARE.
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
