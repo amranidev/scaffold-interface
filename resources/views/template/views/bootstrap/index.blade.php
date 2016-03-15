8f0b41c0de9cb3d707f6866dbb979af2ef7e0534
Amrani <amranidev@gmail.com>
2016-03-15 13:16:34 +0000

    change static link

--- resources/views/template/views/bootstrap/index.blade.php
+++ resources/views/template/views/bootstrap/index.blade.php
@@ -96,6 +96,6 @@
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script> var baseURL = "{{$names->open()}}URL::to('/'){{$names->close()}}"</script>
-<script type="text/javascript" src = "/js/AjaxisBootstrap.js"></script>
-<script type="text/javascript" src = "/js/scaffold-interface-js/customA.js"></script>
+<script type="text/javascript" src = "{{$names->open()}} URL::asset('js/AjaxisBootstrap.js'){{$names->close()}}"></script>
+<script type="text/javascript" src = "{{$names->open()}} URL::asset('js/scaffold-interface-js/customA.js'){{$names->close()}}"></script>
 </html>
