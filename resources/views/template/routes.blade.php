835a4f6afc62a57096f152c8442d152682139bf3
Amrani <amranidev@gmail.com>
2016-03-19 18:14:23 +0000

    update routes template

--- resources/views/template/routes.blade.php
+++ resources/views/template/routes.blade.php
@@ -1,7 +1,7 @@
-//{{$names->TableName()}} Resources
-/*******************************************************/
+//{{$names->TableNameSingle()}} Resources
+/********************* {{$names->TableNameSingle()}} ***********************************************/
 Route::resource('{{$names->TableNameSingle()}}','{{$names->TableName()}}Controller');
 Route::post('{{$names->TableNameSingle()}}/{id}/update','{{$names->TableName()}}Controller@update');
 Route::get('{{$names->TableNameSingle()}}/{id}/delete','{{$names->TableName()}}Controller@destroy');
 Route::get('{{$names->TableNameSingle()}}/{id}/deleteMsg','{{$names->TableName()}}Controller@DeleteMsg');
-/********************************************************/
+/********************* {{$names->TableNameSingle()}} ***********************************************/
