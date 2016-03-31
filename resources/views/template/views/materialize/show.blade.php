8b561a5c6edad5bedef05fdba7f2f16c6bf33d83
Amrani <amranidev@gmail.com>
2016-03-31 23:28:55 +0100

    fix typos

--- resources/views/template/views/materialize/show.blade.php
+++ resources/views/template/views/materialize/show.blade.php
@@ -53,6 +53,6 @@
             </table>
         </div>
     </body>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+    <script src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
 </html>
