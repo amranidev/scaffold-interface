a7505edcfe3b9e2a9123c8e15f2336353a7cd448
Amrani <amranidev@gmail.com>
2016-03-31 23:26:32 +0100

    fix template bt show typos

--- resources/views/template/views/bootstrap/show.blade.php
+++ resources/views/template/views/bootstrap/show.blade.php
@@ -55,6 +55,6 @@
             </table>
         </div>
     </body>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </html>
