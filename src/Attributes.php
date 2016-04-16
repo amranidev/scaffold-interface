d515714a2915a37bee714195cbcd334c70bbccfd
Amrani <amranidev@gmail.com>
2016-04-16 17:38:17 +0100

    change getAttributes

--- src/Attributes.php
+++ src/Attributes.php
@@ -39,15 +39,7 @@ class Attributes
     public function getAttributes()
     {
         //select all the Attributes from table
-
-        if (env('DB_CONNECTION') == 'mysql') {
-
-            $this->result = DB::select(DB::raw('show columns from `' . $this->table . '`;'));
-
-        } elseif (env('DB_CONNECTION') == 'pgsql') {
-
-            $this->result = DB::select(DB::raw("SELECT column_name FROM information_schema.columns WHERE table_name ='" . $this->table . "';"));
-        }
+        $this->result = DB::select(DB::raw("SELECT column_name FROM information_schema.columns WHERE table_schema ='" . env('DB_DATABASE') . "' and table_name ='" . $this->table . "';"));
 
         //delete the first element.(ignore the id section)
         unset($this->result[0]);
