e2edaac370612564ad8eeb71ebcce2e4391e9e60
Amrani <amranidev@gmail.com>
2016-04-18 18:06:40 +0100

    add select postgreSql

--- src/Attributes.php
+++ src/Attributes.php
@@ -44,13 +44,22 @@ class Attributes
      */
     public function getAttributes()
     {
-        //select all the Attributes from table
-        $this->result = DB::select(DB::raw("SELECT column_name FROM information_schema.columns WHERE table_schema ='" . env('DB_DATABASE') . "' and table_name ='" . $this->table . "';"));
+        //if PostgreSql
+        if (env('DB_CONNECTION') == 'pgsql') {
 
+            $this->result = DB::select(DB::raw("SELECT column_name FROM information_schema.columns WHERE table_name ='" . $this->table . "';"));
+        
+        //if Mysql
+        } elseif(env('DB_CONNECTION') == 'mysql') {
+        
+            $this->result = DB::select(DB::raw("SELECT column_name FROM information_schema.columns WHERE table_schema ='" . env('DB_DATABASE') . "' and table_name ='" . $this->table . "';"));
+        
+        }
+        
         //delete the first element.(ignore the id section)
         unset($this->result[0]);
 
         //get result
-        return $this->result;
+        return $this->result
     }
 }
