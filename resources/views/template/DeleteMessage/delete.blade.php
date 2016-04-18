cf9899df3fdda0e0cc7119eaa89577656697e3a1
Amrani <amranidev@gmail.com>
2016-04-18 19:56:10 +0100

    update delete.blade.php

--- resources/views/template/DeleteMessage/delete.blade.php
+++ resources/views/template/DeleteMessage/delete.blade.php
@@ -1,4 +1,5 @@
 <div class="container">
 	<h2 class="thin">Whoops!!</h2>
-	<h3 class="thin">Please rollback the <strong><i>{{$table}}</i></strong> schema!</h3>
+	<h5 class = "thin">Seems like you want to Rollback <strong><i>{{$table}}</i> files</strong></h5>
+	<h5 class="thin">Please rollback the <strong><i>{{$table}}</i></strong> schema and try again!!</h5>
 </div>
