dc0a57a0b7fdc831f28b3f36d21e485d0380bcc6
Amrani <amranidev@gmail.com>
2016-04-21 23:05:08 +0100

    create config file

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+    /*
+     |-----------------------------------------------------------------
+     | Default Files Storage , (Model , Views , Controller , Migration)
+     |-----------------------------------------------------------------
+     |
+     | Here is where you can register you storage paths.
+     |
+     |*/
+
+  return [
+    
+        'model' => app_path(),
+
+        'views' => base_path('resources/views'),
+        
+        'controller' => app_path('Http/Controllers'),
+        
+        'migration' => database_path('migration')
+    
+    ];
