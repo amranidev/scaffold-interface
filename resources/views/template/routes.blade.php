0caa2326d4d1e94d32d377ad190e90dc3cc5b0fc
Amrani <amranidev@gmail.com>
2016-04-23 17:44:46 +0100

    add config vars to routes

--- resources/views/template/routes.blade.php
+++ resources/views/template/routes.blade.php
@@ -1,7 +1,8 @@
 //{{$names->TableNameSingle()}} Resources
 /********************* {{$names->TableNameSingle()}} ***********************************************/
-Route::resource('{{$names->TableNameSingle()}}','{{$names->TableName()}}Controller');
-Route::post('{{$names->TableNameSingle()}}/{id}/update','{{$names->TableName()}}Controller@update');
-Route::get('{{$names->TableNameSingle()}}/{id}/delete','{{$names->TableName()}}Controller@destroy');
-Route::get('{{$names->TableNameSingle()}}/{id}/deleteMsg','{{$names->TableName()}}Controller@DeleteMsg');
+Route::resource('{{$names->TableNameSingle()}}','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->TableName()}}Controller');
+Route::post('{{$names->TableNameSingle()}}/{id}/update','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->TableName()}}Controller@update');
+Route::get('{{$names->TableNameSingle()}}/{id}/delete','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->TableName()}}Controller@destroy');
+Route::get('{{$names->TableNameSingle()}}/{id}/deleteMsg','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->TableName()}}Controller@DeleteMsg');
 /********************* {{$names->TableNameSingle()}} ***********************************************/
+
