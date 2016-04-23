cf26818a435a50fc701ca75f28a7243a2ef51330
Amrani <amranidev@gmail.com>
2016-04-23 17:43:06 +0100

    update config.php

--- config/config.php
+++ config/config.php
@@ -1,24 +1,72 @@
 <?php
 
+  return [
+    
     /*
-     |-----------------------------------------------------------------
-     | Default Files Storage , (Model , Views , Controller , Migration)
-     |-----------------------------------------------------------------
+     |--------------------------------------------------------------------
+     | Default Files Storage , (Models , Views , Controllers , Migrations)
+     |--------------------------------------------------------------------
      |
      | Here is where you can register you storage paths.
      |
      |*/
-
-  return [
     
-        'model' => app_path(),
+        'model' => base_path() . '/app',
 
-        'views' => base_path('resources/views'),
+        'views' => base_path() . '/resources/views',
         
-        'controller' => app_path('Http/Controllers'),
-        
-        'migration' => database_path('migrations'),
+        'controller' => base_path() . '/app/Http/Controllers',
 
-        'routes' => app_path('Http/routes.php')
+        'migration' => base_path() . '/database/migrations',
+
+    /*
+     |--------------------------------------------------------------------
+     | Database migration path.
+     |--------------------------------------------------------------------
+     |
+     | Here is where you can register you migrations path to migrate
+     | the schema via artisan command.
+     | 
+     |*/
+
+        'database' => null,
+
+    /*
+     |-------------------------------------------------------------------
+     | Default route file
+     |-------------------------------------------------------------------
+     |
+     | Here is where you can register your route file.
+     |
+     |*/
+
+        'routes' => base_path() . '/app/Http/routes.php',
     
+    /*
+     |--------------------------------------------------------------------
+     | Package namespace and loaders
+     |--------------------------------------------------------------------
+     |
+     | By default scaffold-interface interact with your app without 
+     | specify any namespace. otherwise, if there is a module or a package 
+     | that you may want scaffold-interface interact with, you must define
+     | namespaces.
+     |
+     |*/
+
+        'controllerNameSpace' => 'App\\Http\\Controllers',
+
+        'modelNameSpace' => 'App',
+
+     /*
+      |-------------------------------------------------------------------
+      | Views loader
+      |-------------------------------------------------------------------
+      |
+      | Here is where you can register your default views loader.
+      | By default is null
+      |
+      |*/
+
+        'loadViews' => null,
     ];
