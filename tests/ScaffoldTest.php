a6ced7bdc58a97b7ccbfcfc1e0d6ba5c74ce73b4
Amrani <amranidev@gmail.com>
2016-05-28 15:43:52 +0100

    create ScaffoldTest

---
+++ tests/ScaffoldTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Amranidev\ScaffoldInterface\Tests;
+
+use Amranidev\ScaffoldInterface\Scaffold;
+use Amranidev\ScaffoldInterface\Tests\TestCase;
+
+class ScaffoldTest extends TestCase
+{
+    public $request;
+
+    public $scaffold;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->request = ["TableName" => "Article",
+                "template" => "bootstrap",
+                "opt0"     => "String",
+                "attr0"    => "title",
+                "opt1"     => "LongText",
+                "attr1"    => "body",
+                "attr2"    => "String",
+                "opt2"     => "author"];
+
+        $this->scaffold = new Scaffold($this->request);
+
+        $this->scaffold->migration()->model()->controller()->route()->views();
+    }
+
+    /**
+     * test created files.
+     */
+    public function testScaffold()
+    {
+        $this->assertFileExists(base_path()."/resources/views/article/index.blade.php");
+        $this->assertFileExists(base_path()."/resources/views/article/create.blade.php");
+        $this->assertFileExists(base_path()."/resources/views/article/edit.blade.php");
+        $this->assertFileExists(base_path()."/resources/views/article/show.blade.php");
+        $this->assertFileExists(base_path()."/app/Article.php");
+        $this->assertFileExists(base_path()."/app/ArticleController.php");
+        $this->assertFileExists(base_path()."/app/routes.php");
+    }
+}
