67ccaf0f9a62fc323fd75edc823b65aa9ac941c6
Amrani <amranidev@gmail.com>
2016-06-06 02:21:18 +0100

    rename DefualtDatabase to MysqlDatabase

---
+++ src/DataSystem/MysqlDatabase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Amranidev\ScaffoldInterface\DataSystem;
+
+use Illuminate\Support\Facades\DB;
+
+/**
+ * class DefaultDatabase.
+ *
+ * @author Athi Krishnan <athikrishnan5@gmail.com>
+ */
+class MysqlDatabase extends Database
+{
+    public function tableNames()
+    {
+        return collect(DB::select($this->getQuery()))->pluck('Tables_in_scaffold120')->reject(function ($name) {
+                        return $this->skips()->contains($name);
+                    });
+    }
+    
+    public function getQuery()
+    {
+        return 'SHOW TABLES';
+    }
+
+    public function skipNames()
+    {
+        return collect([]);
+    }
+}
