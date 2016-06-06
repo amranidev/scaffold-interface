0fd33d6e66d4c4731d96eae7514a358170d0291b
athithan jayabalakrishnan <athithan jayabalakrishnan>
2016-06-06 19:37:43 +0530

    feature: select env-s in which the scaffold runs.

---
+++ src/Http/Middleware/ScaffoldMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Amranidev\ScaffoldInterface\Http\Middleware;
+
+use Closure;
+
+class ScaffoldMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->segment(1) == 'scaffold') {
+            
+            $allowed  = collect(config('scaffold.env'))
+                            ->contains(config('app.env'));
+
+            if( ! $allowed) {
+                return redirect('/');
+            }
+        }
+
+        return $next($request);
+    }
+}
