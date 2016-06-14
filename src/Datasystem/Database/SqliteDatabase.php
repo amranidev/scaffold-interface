7a0a1abbc73a4b84ad9507c2643e96da5da9f553
Amrani <amranidev@gmail.com>
2016-06-14 23:17:40 +0000

    opt Sqlite

--- src/Datasystem/Database/SqliteDatabase.php
+++ src/Datasystem/Database/SqliteDatabase.php
@@ -9,11 +9,21 @@ namespace Amranidev\ScaffoldInterface\Datasystem\Database;
  */
 class SqliteDatabase extends Database
 {
+    /**
+     * sqlite query.
+     * 
+     * @return string
+     */  
     public function getQuery()
     {
         return "SELECT name FROM sqlite_master WHERE type='table'";
     }
 
+    /**
+     * skip unused schemas.
+     * 
+     * @return /Illuminate/Support/Collection
+     */ 
     public function skipNames()
     {
         return collect([
