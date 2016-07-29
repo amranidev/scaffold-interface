eb07d15192bbd5edb3f8a76712f5923d6eeabaee
Amrani <amranidev@gmail.com>
2016-07-29 18:37:58 +0100

    formating

--- resources/views/template/HomePage/HomePage.blade.php
+++ resources/views/template/HomePage/HomePage.blade.php
@@ -24,322 +24,120 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 	</head>
-</head>
-<body class="hold-transition skin-blue sidebar-mini">
-	<div class="wrapper">
-		<header class="main-header">
-			<!-- Logo -->
-			<a href="index2.html" class="logo">
-				<!-- mini logo for sidebar mini 50x50 pixels -->
-				<span class="logo-mini"><b>E</b>CM</span>
-				<!-- logo for regular state and mobile devices -->
-				<span class="logo-lg"><b>Admin</b>LTE</span>
-			</a>
-			<!-- Header Navbar: style can be found in header.less -->
-			<nav class="navbar navbar-static-top">
-				<!-- Sidebar toggle button-->
-				<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-					<span class="sr-only">Toggle navigation</span>
+	<body class="hold-transition skin-blue sidebar-mini">
+		<div class="wrapper">
+			<header class="main-header">
+				<!-- Logo -->
+				<a href="index2.html" class="logo">
+					<!-- mini logo for sidebar mini 50x50 pixels -->
+					<span class="logo-mini"><b>E</b>CM</span>
+					<!-- logo for regular state and mobile devices -->
+					<span class="logo-lg"><b>Admin</b>LTE</span>
 				</a>
-				<div class="navbar-custom-menu">
-					<ul class="nav navbar-nav">
-						<!-- Messages: style can be found in dropdown.less-->
-						<li class="dropdown messages-menu">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-								<i class="fa fa-envelope-o"></i>
-								<span class="label label-success">4</span>
-							</a>
-							<ul class="dropdown-menu">
-								<li class="header">You have 4 messages</li>
-								<li>
-									<!-- inner menu: contains the actual data -->
-									<ul class="menu">
-										<li><!-- start message -->
-										<a href="#">
-											<div class="pull-left">
-												<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-											Support Team
-											<small><i class="fa fa-clock-o"></i> 5 mins</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<!-- end message -->
-									<li>
-										<a href="#">
-											<div class="pull-left">
-												<img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-											AdminLTE Design Team
-											<small><i class="fa fa-clock-o"></i> 2 hours</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<div class="pull-left">
-												<img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-											Developers
-											<small><i class="fa fa-clock-o"></i> Today</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<div class="pull-left">
-												<img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-											Sales Department
-											<small><i class="fa fa-clock-o"></i> Yesterday</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<div class="pull-left">
-												<img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-											Reviewers
-											<small><i class="fa fa-clock-o"></i> 2 days</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-								</ul>
-							</li>
-							<li class="footer"><a href="#">See All Messages</a></li>
-						</ul>
-					</li>
-					<!-- Notifications: style can be found in dropdown.less -->
-					<li class="dropdown notifications-menu">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-bell-o"></i>
-							<span class="label label-warning">10</span>
-						</a>
-						<ul class="dropdown-menu">
-							<li class="header">You have 10 notifications</li>
-							<li>
-								<!-- inner menu: contains the actual data -->
-								<ul class="menu">
-									<li>
-										<a href="#">
-											<i class="fa fa-users text-aqua"></i> 5 new members joined today
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-											page and may cause design problems
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-users text-red"></i> 5 new members joined
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-shopping-cart text-green"></i> 25 sales made
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="fa fa-user text-red"></i> You changed your username
-										</a>
-									</li>
-								</ul>
-							</li>
-							<li class="footer"><a href="#">View all</a></li>
-						</ul>
-					</li>
-					<!-- Tasks: style can be found in dropdown.less -->
-					<li class="dropdown tasks-menu">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-flag-o"></i>
-							<span class="label label-danger">9</span>
-						</a>
-						<ul class="dropdown-menu">
-							<li class="header">You have 9 tasks</li>
-							<li>
-								<!-- inner menu: contains the actual data -->
-								<ul class="menu">
-									<li><!-- Task item -->
-									<a href="#">
-										<h3>
-										Design some buttons
-										<small class="pull-right">20%</small>
-										</h3>
-										<div class="progress xs">
-											<div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-												<span class="sr-only">20% Complete</span>
-											</div>
-										</div>
-									</a>
-								</li>
-								<!-- end task item -->
-								<li><!-- Task item -->
-								<a href="#">
-									<h3>
-									Create a nice theme
-									<small class="pull-right">40%</small>
-									</h3>
-									<div class="progress xs">
-										<div class="progress-bar progress-bar-green" style="width: 40%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-											<span class="sr-only">40% Complete</span>
-										</div>
-									</div>
-								</a>
-							</li>
-							<!-- end task item -->
-							<li><!-- Task item -->
+				<!-- Header Navbar: style can be found in header.less -->
+				<nav class="navbar navbar-static-top">
+					<!-- Sidebar toggle button-->
+					<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+						<span class="sr-only">Toggle navigation</span>
+					</a>
+				</nav>
+			</header>
+			<aside class="main-sidebar">
+				<!-- sidebar: style can be found in sidebar.less -->
+				<section class="sidebar">
+					<!-- Sidebar user panel -->
+					<div class="user-panel">
+						<div class="pull-left image">
+							<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+						</div>
+						<div class="pull-left info">
+							<p>Alexander Pierce</p>
+							<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+						</div>
+					</div>
+					<!-- search form -->
+					<form action="#" method="get" class="sidebar-form">
+						<div class="input-group">
+							<input type="text" name="q" class="form-control" placeholder="Search...">
+							<span class="input-group-btn">
+								<button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+								</button>
+							</span>
+						</div>
+					</form>
+					<!-- /.search form -->
+					<!-- sidebar menu: : style can be found in sidebar.less -->
+					<ul class="sidebar-menu">
+						<li class="header">MAIN NAVIGATION</li>
+						<li class="active treeview">
 							<a href="#">
-								<h3>
-								Some task I need to do
-								<small class="pull-right">60%</small>
-								</h3>
-								<div class="progress xs">
-									<div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-										<span class="sr-only">60% Complete</span>
-									</div>
-								</div>
+								<i class="fa fa-dashboard"></i> <span>Dashboard</span></i>
 							</a>
 						</li>
-						<!-- end task item -->
-						<li><!-- Task item -->
-						<a href="#">
-							<h3>
-							Make beautiful transitions
-							<small class="pull-right">80%</small>
-							</h3>
-							<div class="progress xs">
-								<div class="progress-bar progress-bar-yellow" style="width: 80%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-									<span class="sr-only">80% Complete</span>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-files-o"></i>
+								<span>Administration</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="{{url('/')}}"><i class="fa fa-circle-o"></i> Users</a></li>
+								<li><a href="{{url('/')}}"><i class="fa fa-circle-o"></i> Roles</a></li>
+							</ul>
+						</li>
+					</ul>
+				</section>
+				<!-- /.sidebar -->
+			</aside>
+			<div class="content-wrapper">
+				<section class="content-header">
+					<h1>
+					Dashboard
+					<small>Control panel</small>
+					</h1>
+					<ol class="breadcrumb">
+						<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+						<li class="active">Dashboard</li>
+					</ol>
+				</section>
+				<section class="content">
+					<!-- Small boxes (Stat box) -->
+					<div class="row">
+						<div class="col-lg-3 col-xs-6">
+							<!-- small box -->
+							<div class="small-box bg-aqua">
+								<div class="inner">
+									<h3>150</h3>
+									<p>Users</p>
 								</div>
+								<div class="icon">
+									<i class="ion ion-person-stalker"></i>
+								</div>
+								<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
 							</div>
-						</a>
-					</li>
-					<!-- end task item -->
-				</ul>
-			</li>
-			<li class="footer">
-				<a href="#">View all tasks</a>
-			</li>
-		</ul>
-	</li>
-	<!-- User Account: style can be found in dropdown.less -->
-	<li class="dropdown user user-menu">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-			<img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-			<span class="hidden-xs">Alexander Pierce</span>
-		</a>
-		<ul class="dropdown-menu">
-			<!-- User image -->
-			<li class="user-header">
-				<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-				<p>
-					Alexander Pierce - Web Developer
-					<small>Member since Nov. 2012</small>
-				</p>
-			</li>
-			<!-- Menu Body -->
-			<li class="user-body">
-				<div class="row">
-					<div class="col-xs-4 text-center">
-						<a href="#">Followers</a>
+						</div>
+						@foreach($Parse as $entity)
+						<div class="col-lg-3 col-xs-6">
+							<!-- small box -->
+							<div class="small-box bg-green">
+								<div class="inner">
+									<h3>150</h3>
+									<p>{{$entity->tablename}}</p>
+								</div>
+								<div class="icon">
+									<i class="ion ion-person-stalker"></i>
+								</div>
+								<a href="{{url('/')}}/{{lcfirst(str_singular($entity->tablename))}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+						@endforeach
 					</div>
-					<div class="col-xs-4 text-center">
-						<a href="#">Sales</a>
-					</div>
-					<div class="col-xs-4 text-center">
-						<a href="#">Friends</a>
-					</div>
-				</div>
-				<!-- /.row -->
-			</li>
-			<!-- Menu Footer-->
-			<li class="user-footer">
-				<div class="pull-left">
-					<a href="#" class="btn btn-default btn-flat">Profile</a>
-				</div>
-				<div class="pull-right">
-					<a href="#" class="btn btn-default btn-flat">Sign out</a>
-				</div>
-			</li>
-		</ul>
-	</li>
-	<!-- Control Sidebar Toggle Button -->
-	<li>
-		<a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-	</li>
-</ul>
-</div>
-</nav>
-</header>
-  <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-      <!-- Sidebar user panel -->
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p>Alexander Pierce</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-      <!-- /.search form -->
-      <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu">
-        <li class="header">MAIN NAVIGATION</li>
-        <li class="active treeview">
-          <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
-          </a>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Administration</span>
-            <span class="label label-primary pull-right">4</span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Users</a></li>
-            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Roles</a></li>
-          </ul>
-        </li>
-      </ul>
-    </section>
-    <!-- /.sidebar -->
-  </aside>
-  <div class="content-wrapper">
-  	<h1>Hi</h1>
-  </div>
-</div>
-<!-- Compiled and minified JavaScript -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.5/js/app.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.5/js/demo.js"></script>
-</body>
+				</section>
+			</div>
+		</div>
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.5/js/app.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.5/js/demo.js"></script>
+	</body>
 </html>
