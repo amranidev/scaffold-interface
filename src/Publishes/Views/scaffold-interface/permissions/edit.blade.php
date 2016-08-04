be68ff65c39ae88bd2b633ab75b27207b152ad11
Amrani <amranidev@gmail.com>
2016-08-04 19:32:39 +0100

    create permission views

---
+++ src/Publishes/Views/scaffold-interface/permissions/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('scaffold-interface.layouts.app')
+@section('content')
+<section class="content">
+	<div class="box box-primary">
+		<div class="box-header">
+			<h3>Edit Permission</h3>
+		</div>
+		<div class="box-body">
+			<form action="{{url('permissions/update')}}" method = "post">
+				{!! csrf_field() !!}
+				<input type="hidden" name = "permission_id" value = "{{$permission->id}}">
+				<div class="form-group">
+				<label for="">permission</label>
+					<input type="text" name = "name" class = "form-control" placeholder = "Name" value = "{{$permission->name}}">
+				</div>
+				<div class="box-footer">
+					<button class = 'btn btn-primary' type = "submit">Update</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
+@endsection
---
+++ src/Publishes/Views/scaffold-interface/permissions/index.blade.php
@@ -0,0 +1,30 @@
+@extends('scaffold-interface.layouts.app')
+@section('content')
+<section class="content">
+	<div class="box box-primary">
+		<div class="box-header">
+			<h3>All Permissions</h3>
+		</div>
+		<div class="box-body">
+			<a href="{{url('permissions/create')}}" class = "btn btn-success">create new permission</a>
+			<table class="table table-striped">
+				<head>
+					<th>Permission</th>
+					<th>Actions</th>
+				</head>
+				<tbody>
+					@foreach($permissions as $permission)
+					<tr>
+						<td>{{$permission->name}}</td>
+						<td>
+							<a href="{{url('/permissions/edit')}}/{{$permission->id}}" class = "btn btn-primary">edit</a>
+							<a href="{{url('/permissions/delete')}}/{{$permission->id}}" class = "btn btn-danger">delete</a>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
+</section>
+@endsection
