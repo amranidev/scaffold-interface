1414178dc44c3ab559edadf14eec279a7df3d472
Amrani <amranidev@gmail.com>
2016-08-05 19:20:08 +0100

    add necessary markup roles-permissions

--- src/Publishes/Views/scaffold-interface/users/edit.blade.php
+++ src/Publishes/Views/scaffold-interface/users/edit.blade.php
@@ -11,18 +11,94 @@
 				<input type="hidden" name = "user_id" value = "{{$user->id}}">
 				<div class="form-group">
 					<label for="">Email</label>
-					<input type="email" name = "email" value = "{{$user->email}}" class = "form-control">
+					<input type="email" name = "email" value = "{{$user->email}}" class = "form-control" required>
 				</div>
 				<div class="form-group">
 					<label for="">Name</label>
-					<input type="text" name = "name" value = "{{$user->name}}" class = "form-control">
+					<input type="text" name = "name" value = "{{$user->name}}" class = "form-control" required>
 				</div>
 				<div class="form-group">
 					<label for="">Password</label>
-					<input type="password" name = "password" class = "form-control" placeholder = "password">
+					<input type="password" name = "password" class = "form-control" placeholder = "password" required>
 				</div>
 				<button class = "btn btn-primary" type="submit">Update</button>
 			</form>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div class="box box-primary">
+				<div class="box-header">
+					<h3>{{$user->name}} Roles</h3>
+				</div>
+				<div class="box-body">
+					<form action="{{url('users/addRole')}}" method = "post">
+						{!! csrf_field() !!}
+						<input type="hidden" name = "user_id" value = "{{$user->id}}">
+						<div class="form-group">
+							<select name="role_name" id="" class = "form-control">
+								@foreach($roles as $role)
+								<option value="{{$role}}">{{$role}}</option>
+								@endforeach
+							</select>
+						</div>
+						<div class="form-group">
+							<button class = 'btn btn-primary'>Add role</button>
+						</div>
+					</form>
+					<table class = 'table'>
+						<thead>
+							<th>Role</th>
+							<th>Action</th>
+						</thead>
+						<tbody>
+							@foreach($userRoles as $role)
+							<tr>
+								<td>{{$role->name}}</td>
+								<td><a href="" class = "btn btn-danger">delete</a></td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="box box-primary">
+				<div class="box-header">
+					<h3>{{$user->name}} Permissions</h3>
+				</div>
+				<div class="box-body">
+					<form action="{{url('users/addPermission')}}" method = "post">
+						{!! csrf_field() !!}
+						<input type="hidden" name = "user_id" value = "{{$user->id}}">
+						<div class="form-group">
+							<select name="permission_name" id="" class = "form-control">
+								@foreach($permissions as $permission)
+								<option value="{{$permission}}">{{$permission}}</option>
+								@endforeach
+							</select>
+						</div>
+						<div class="form-group">
+							<button class = 'btn btn-primary'>Add permission</button>
+						</div>
+					</form>
+					<table class = 'table'>
+						<thead>
+							<th>Permission</th>
+							<th>Action</th>
+						</thead>
+						<tbody>
+							@foreach($userPermissions as $permission)
+							<tr>
+								<td>{{$permission->name}}</td>
+								<td><a href="" class = "btn btn-danger">delete</a></td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
