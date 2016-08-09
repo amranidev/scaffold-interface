171bed579bd010ef4d8c59b2bcee778e5af2f6af
Amrani <amranidev@gmail.com>
2016-08-09 20:08:33 +0100

    update layouts

--- src/Publishes/Views/scaffold-interface/layouts/app.blade.php
+++ src/Publishes/Views/scaffold-interface/layouts/app.blade.php
@@ -40,6 +40,52 @@
 					<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
 						<span class="sr-only">Toggle navigation</span>
 					</a>
+					<div class="navbar-custom-menu">
+						<ul class="nav navbar-nav">
+							<li class="dropdown user user-menu">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+									<img src="http://ahloman.net/wp-content/uploads/2013/06/user.jpg" class="user-image" alt="User Image">
+									<span class="hidden-xs">{{Auth::user()->name}}</span>
+								</a>
+								<ul class="dropdown-menu">
+									<!-- User image -->
+									<li class="user-header">
+										<img src="http://ahloman.net/wp-content/uploads/2013/06/user.jpg" class="img-circle" alt="User Image">
+										<p>
+											{{Auth::user()->name}}
+										</p>
+									</li>
+									<!-- Menu Body -->
+									<li class="user-body">
+										<div class="row">
+											<div class="col-xs-4 text-center">
+												<a href="#">Followers</a>
+											</div>
+											<div class="col-xs-4 text-center">
+												<a href="#">Sales</a>
+											</div>
+											<div class="col-xs-4 text-center">
+												<a href="#">Friends</a>
+											</div>
+										</div>
+										<!-- /.row -->
+									</li>
+									<!-- Menu Footer-->
+									<li class="user-footer">
+										<div class="pull-left">
+											<a href="#" class="btn btn-default btn-flat">Profile</a>
+										</div>
+										<div class="pull-right">
+											<a href="{{url('logout')}}" class="btn btn-default btn-flat">Sign out</a>
+										</div>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+							</li>
+						</ul>
+					</div>
 				</nav>
 			</header>
 			<aside class="main-sidebar">
@@ -64,23 +110,16 @@
 								<i class="fa fa-dashboard"></i> <span>Dashboard</span></i>
 							</a>
 						</li>
-						<li class="treeview">
-							<a href="#">
-								<i class="fa fa-files-o"></i>
-								<span>Administration</span>
-							</a>
-							<ul class="treeview-menu">
-								<li><a href="{{url('/users')}}"><i class="fa fa-circle-o"></i> Users</a></li>
-								<li><a href="{{url('/roles')}}"><i class="fa fa-circle-o"></i> Roles</a></li>
-								<li><a href="{{url('/permissions')}}"><i class="fa fa-circle-o"></i> Permissions</a></li>
-							</ul>
-						</li>
+						<li class="header">ADMINISTRATOR</li>
+						<li><a href="{{url('/users')}}"><i class="fa fa-users"></i> Users</a></li>
+						<li><a href="{{url('/roles')}}"><i class="fa fa-user-plus"></i> Roles</a></li>
+						<li><a href="{{url('/permissions')}}"><i class="fa fa-key"></i> Permissions</a></li>
 					</ul>
 				</section>
 				<!-- /.sidebar -->
 			</aside>
 			<div class="content-wrapper">
-			@yield('content')
+				@yield('content')
 			</div>
 		</div>
 		<!-- Compiled and minified JavaScript -->
