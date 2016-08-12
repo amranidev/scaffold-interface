6d45cc7bc8460bd6db09cf5dbf5f064676a7af29
Amrani <amranidev@gmail.com>
2016-08-12 23:36:46 +0100

    opt

--- src/Publishes/Controllers/ScaffoldInterface/UserController.php
+++ src/Publishes/Controllers/ScaffoldInterface/UserController.php
@@ -10,6 +10,11 @@ use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
     public function index()
     {
         $users = \App\User::all();
@@ -17,11 +22,22 @@ class UserController extends Controller
         return view('scaffold-interface.users.index', compact('users'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('scaffold-interface.users.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $user = new \App\User();
@@ -35,6 +51,11 @@ class UserController extends Controller
         return redirect('users');
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
         $user = \App\User::findOrfail($id);
@@ -46,6 +67,12 @@ class UserController extends Controller
         return view('scaffold-interface.users.edit', compact('user', 'roles', 'permissions', 'userRoles', 'userPermissions'));
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param   \Illuminate\Http\Request  $request
+     * @return  \Illuminate\Http\Response
+     */
     public function update(Request $request)
     {
         $user = \App\User::findOrfail($request->user_id);
@@ -59,6 +86,12 @@ class UserController extends Controller
         return redirect('users');
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param   int  $id
+     * @return  \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $user = \App\User::findOrfail($id);
@@ -68,6 +101,12 @@ class UserController extends Controller
         return redirect('users');
     }
 
+    /**
+     * Assign Role to user.
+     * 
+     * @param \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
     public function addRole(Request $request)
     {
         $user = \App\User::findOrfail($request->user_id);
@@ -76,6 +115,12 @@ class UserController extends Controller
         return redirect('users/edit/'.$request->user_id);
     }
 
+    /**
+     * Assign Permission to a user.
+     * 
+     * @param \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
     public function addPermission(Request $request)
     {
         $user = \App\User::findorfail($request->user_id);
@@ -84,6 +129,12 @@ class UserController extends Controller
         return redirect('users/edit/'.$request->user_id);
     }
 
+    /**
+     * revoke Permission to a user.
+     * 
+     * @param \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
     public function revokePermission($permission, $user_id)
     {
         $user = \App\User::findorfail($user_id);
@@ -93,6 +144,12 @@ class UserController extends Controller
         return redirect('users/edit/'.$user_id);
     }
 
+    /**
+     * revoke Role to a a user.
+     * 
+     * @param \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
     public function revokeRole($role, $user_id)
     {
         $user = \App\User::findorfail($user_id);
