4e2a7d52de85c569cbdd955b1d3ad9483c4d5e43
Amrani <amranidev@gmail.com>
2016-08-13 20:12:15 +0100

    add some commemts to RoleController

--- src/Publishes/Controllers/ScaffoldInterface/RoleController.php
+++ src/Publishes/Controllers/ScaffoldInterface/RoleController.php
@@ -8,6 +8,11 @@ use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
     public function index()
     {
         $roles = Role::all();
@@ -15,11 +20,22 @@ class RoleController extends Controller
         return view('scaffold-interface.roles.index', compact('roles'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('scaffold-interface.roles.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param   \Illuminate\Http\Request  $request
+     * @return  \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         Role::create(['name' => $request->name]);
@@ -27,6 +43,11 @@ class RoleController extends Controller
         return redirect('roles');
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
         $role = Role::findOrFail($id);
@@ -34,6 +55,12 @@ class RoleController extends Controller
         return view('scaffold-interface.roles.edit', compact('role'));
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param   \Illuminate\Http\Request  $request
+     * @return  \Illuminate\Http\Response
+     */
     public function update(Request $request)
     {
         $role = Role::findOrFail($request->role_id);
@@ -45,6 +72,12 @@ class RoleController extends Controller
         return redirect('roles');
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param   int  $id
+     * @return  \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $role = Role::findOrFail($id);
