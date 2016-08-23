39f801ff60a2e8990056b802d7291d5971fc6576
Amrani <amranidev@gmail.com>
2016-08-23 18:26:31 +0100

    create pivot template

---
+++ resources/views/template/ManyToMany/migration.blade.php
@@ -0,0 +1,35 @@
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class {{ucfirst($first)}}{{ucfirst($second)}} extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('{{$first}}_{{$second}}',function (Blueprint $table){
+			$table->increments('id')->unique()->index()->unsigned();
+			$table->integer('{{str_singular($first)}}_id')->unsigned()->index();
+			$table->foreign('{{str_singular($first)}}_id')->references('id')->on('{{$first}}')->onDelete('cascade');
+			$table->integer('{{str_singular($second)}}_id')->unsigned()->index();
+			$table->foreign('{{str_singular($second)}}_id')->references('id')->on('{{$second}}')->onDelete('cascade');
+			/**
+			 * Type your addition here
+			 *
+			 */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('{{$first}}_{{$second}}');
+    }
+}
