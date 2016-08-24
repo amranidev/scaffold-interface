9cbfa4903d6b841d5426f76bc575671268139791
Amrani <amranidev@gmail.com>
2016-08-24 18:41:21 +0100

    create model template ManyToMany

---
+++ resources/views/template/ManyToMany/model.blade.php
@@ -0,0 +1,25 @@
+    public function {{str_plural($model)}}()
+    {
+        return $this->belongsToMany('App\{{ucfirst(str_singular($model))}}');
+    }
+
+    /**
+     * Assign a {{str_singular($model)}}.
+     *
+     * @param ${{str_singular($model)}}
+     * @return mixed
+     */
+    public function assign{{ucfirst(str_singular($model))}}(${{str_singular($model)}})
+    {
+        return $this->{{str_plural($model)}}()->attach(${{str_singular($model)}});
+    }
+    /**
+     * Remove a {{str_singular($model)}}.
+     *
+     * @param ${{str_singular($model)}}
+     * @return mixed
+     */
+    public function remove{{ucfirst(str_singular($model))}}(${{str_singular($model)}})
+    {
+        return $this->{{str_plural($model)}}()->detach(${{str_singular($model)}});
+    }
