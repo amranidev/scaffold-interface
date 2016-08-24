a6616be097f72657cd54f698ba7be14d86075be2
Amrani Houssain <amranidev@gmail.com>
2016-08-24 17:46:53 +0000

    Applied fixes from StyleCI

--- src/ManyToMany/ManyToMany.php
+++ src/ManyToMany/ManyToMany.php
@@ -20,7 +20,7 @@ class ManyToMany extends Filesystem
         $this->request = $request;
         $this->tables = $this->attributes();
     }
-    
+
     public function attributes()
     {
         $result = [];
@@ -40,8 +40,8 @@ class ManyToMany extends Filesystem
 
     public function model()
     {
-        $this->relationship(app_path(ucfirst(str_singular($this->tables['first']))) .".php", $this->tables['second']);
-        $this->relationship(app_path(ucfirst(str_singular($this->tables['second']))) .".php", $this->tables['first']);
+        $this->relationship(app_path(ucfirst(str_singular($this->tables['first']))).'.php', $this->tables['second']);
+        $this->relationship(app_path(ucfirst(str_singular($this->tables['second']))).'.php', $this->tables['first']);
     }
 
     public function migration()
@@ -49,18 +49,18 @@ class ManyToMany extends Filesystem
         $migrationContent = view('scaffold-interface::template.ManyToMany.migration', $this->tables)->render();
 
         $migrationFileName = date('Y').'_'.date('m').'_'.date('d').'_'.date('his').'_'.ucfirst($this->tables['first']).ucfirst($this->tables['second']).'.php';
- 
+
         $this->make(config('amranidev.config.migration').'/'.$migrationFileName, $migrationContent);
     }
 
     public function relationship($path, $model)
     {
         $lines = file($path, FILE_IGNORE_NEW_LINES);
-        $last = sizeof($lines) - 1;
+        $last = count($lines) - 1;
         unset($lines[$last]);
         $result = implode("\n", array_values($lines));
         $model = view('scaffold-interface::template.ManyToMany.model', ['model' => $model])->render();
-        $result = $result. "\n\n\t" . $model . "\n" . '}' . "\n";
+        $result = $result."\n\n\t".$model."\n".'}'."\n";
 
         return file_put_contents($path, $result);
     }
