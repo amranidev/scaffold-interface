d0c3910ba0b3f69cb0a1447b5902d899892dfc0f
Amrani <amranidev@gmail.com>
2016-09-07 16:53:53 +0100

    enhance dashboard view

--- src/Publishes/Views/scaffold-interface/dashboard/dashboard.blade.php
+++ src/Publishes/Views/scaffold-interface/dashboard/dashboard.blade.php
@@ -16,13 +16,39 @@
 			<!-- small box -->
 			<div class="small-box bg-aqua">
 				<div class="inner">
-					<h3>{{$users->count()}}</h3>
+					<h3>{{$users}}</h3>
 					<p>Users</p>
 				</div>
 				<div class="icon">
 					<i class="ion ion-person-stalker"></i>
 				</div>
 				<a href="{{url('users')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-xs-6">
+			<!-- small box -->
+			<div class="small-box bg-red">
+				<div class="inner">
+					<h3>{{$roles}}</h3>
+					<p>Roles</p>
+				</div>
+				<div class="icon">
+					<i class="fa fa-user-plus"></i>
+				</div>
+				<a href="{{url('roles')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-lg-3 col-xs-6">
+			<!-- small box -->
+			<div class="small-box bg-yellow">
+				<div class="inner">
+					<h3>{{$permissions}}</h3>
+					<p>Permissions</p>
+				</div>
+				<div class="icon">
+					<i class="fa fa-key"></i>
+				</div>
+				<a href="{{url('permissions')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
 			</div>
 		</div>
 	</div>
