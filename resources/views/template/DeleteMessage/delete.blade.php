8d0a56cb34e62671f2be71bfff8896945f595436
Amrani <amranidev@gmail.com>
2016-10-02 23:49:51 +0100

    enhance delete message template

--- resources/views/template/DeleteMessage/delete.blade.php
+++ resources/views/template/DeleteMessage/delete.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
 	<h2 class="thin">Whoops!!</h2>
-	<h5 class = "thin">Seems like you want to Rollback <strong><i>{{$table}}</i> files</strong></h5>
-	<h5 class="thin">Please rollback the <strong><i>{{$table}}</i></strong> schema and try again!!</h5>
+	<h5 class = "thin">Seems like you want to Delete <strong><i>{{$table}}</i> MVC files</strong></h5>
+	<h5 class="thin">Please rollback <strong><i>{{$table}}</i></strong> schema and try again!!</h5>
 </div>
