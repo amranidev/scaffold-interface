0362c72aa6040d615345f2351c3dc8f2cdda75be
Amrani <amranidev@gmail.com>
2016-10-29 17:29:01 +0100

    :new: change Namesgenerate to Parser

---
+++ src/Parsers/Parser.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Amranidev\ScaffoldInterface\Parsers;
+
+/**
+ * class Parser
+ * 
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
+class Parser
+{
+	/**
+	 * parser data
+	 * 
+	 * @var array $data
+	 */ 
+	protected $data;
+
+	/**
+	 * Create new Parser instance
+	 * 
+	 * @return void
+	 */ 
+	public function __construct($request)
+	{
+		$this->data = $request;
+	}
+
+	/**
+	 * Get the entity name singular.
+	 * 
+	 * @return string
+	 */ 
+	public function singular()
+	{
+		 return str_singular(str_slug($this->data['TableName'], '_'));
+	}
+
+	/**
+	 * Get the entity name plural.
+	 *
+	 * @return string
+	 */  
+	public function plural()
+	{
+		return str_plural(str_slug($this->data['TableName'], '_'));
+	}
+
+	/**
+     * Get Template.
+     *
+     * @return string
+     */
+    public function getTemplate()
+    {
+        return $this->data['template'];
+    }
+
+    /**
+     * Parse template specification.
+     *
+     * @throws Exception
+     *
+     * @return string
+     */
+    public function getParse()
+    {
+        if (starts_with($this->data['template'], 'boot')) {
+            return 'Bt';
+        } else {
+            return 'Mt';
+        }
+
+        throw new \Exception('Template Error');
+    }
+}
