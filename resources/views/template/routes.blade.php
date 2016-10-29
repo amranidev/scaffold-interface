ba7b22f3e4bd84f878c2f9161a25dce74df47729
Amrani <amranidev@gmail.com>
2016-10-29 17:24:01 +0100

    update reouts template

--- resources/views/template/routes.blade.php
+++ resources/views/template/routes.blade.php
@@ -1,8 +1,8 @@
-//{{$names->tableNameSingle()}} Resources
-/********************* {{$names->tableNameSingle()}} ***********************************************/
-Route::resource('{{$names->tableNameSingle()}}','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->tableName()}}Controller');
-Route::post('{{$names->tableNameSingle()}}/{id}/update','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->tableName()}}Controller@update');
-Route::get('{{$names->tableNameSingle()}}/{id}/delete','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->tableName()}}Controller@destroy');
-Route::get('{{$names->tableNameSingle()}}/{id}/deleteMsg','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{$names->tableName()}}Controller@DeleteMsg');
-/********************* {{$names->tableNameSingle()}} ***********************************************/
+//{{$parser->singular()}} Resources
+/********************* {{$parser->singular()}} ***********************************************/
+Route::resource('{{$parser->singular()}}','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{ucfirst($parser->singular())}}Controller');
+Route::post('{{$parser->singular()}}/{id}/update','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{ucfirst($parser->singular())}}Controller@update');
+Route::get('{{$parser->singular()}}/{id}/delete','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{ucfirst($parser->singular())}}Controller@destroy');
+Route::get('{{$parser->singular()}}/{id}/deleteMsg','@if(config('amranidev.config.controllerNameSpace'))\{{config('amranidev.config.controllerNameSpace')}}\@endif{{ucfirst($parser->singular())}}Controller@DeleteMsg');
+/********************* {{$parser->singular()}} ***********************************************/
 
