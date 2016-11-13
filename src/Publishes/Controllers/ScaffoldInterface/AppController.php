106869417b8586840421e2416c2d185482ca7254
Amrani <amranidev@gmail.com>
2016-11-13 16:37:44 +0000

    Create AppController

---
+++ src/Publishes/Controllers/ScaffoldInterface/AppController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\ScaffoldInterface;
+
+use App\Http\Controllers\Controller;
+
+/**
+  * Class AppController 
+  *
+  * @author Amrani Houssain <amranidev@gmail.com>
+  */ 
+class AppController extends Controller
+{
+	/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+	public function dashboard()
+	{
+        $users = \App\User::all()->count();
+        $roles = \Spatie\Permission\Models\Role::all()->count();
+        $permissions = \Spatie\Permission\Models\Permission::all()->count();
+        $entities = \Amranidev\ScaffoldInterface\Scaffoldinterface::all();
+
+        return view('scaffold-interface.dashboard.dashboard', compact('users', 'roles', 'permissions', 'entities'));
+	}
+}
