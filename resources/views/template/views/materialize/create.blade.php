6954fac6a0e230f2c71a963edaa417e4c04d189a
Amrani <amranidev@gmail.com>
2016-11-18 23:10:13 +0000

    fix token

--- resources/views/template/views/materialize/create.blade.php
+++ resources/views/template/views/materialize/create.blade.php
@@ -8,7 +8,7 @@
     </form>
     <br>
     <form method = 'POST' action = '@{!!url("{{$parser->singular()}}")!!}'>
-        <input type = 'hidden' name = '_token' value = '@{!! Session::token() !!}}'>
+        <input type = 'hidden' name = '_token' value = '@{{ Session::token() }}'>
         @foreach($dataSystem->dataScaffold('v') as $value)
         <div class="input-field col s6">
             <input id="{{$value}}" name = "{{$value}}" type="text" class="validate">
