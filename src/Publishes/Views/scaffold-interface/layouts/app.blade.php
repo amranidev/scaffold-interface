b88d7a4314691dca7a4cc6e4e8785ddebdd38ec0
Amrani <amranidev@gmail.com>
2016-11-22 20:59:45 +0000

    update app blade

--- src/Publishes/Views/scaffold-interface/layouts/app.blade.php
+++ src/Publishes/Views/scaffold-interface/layouts/app.blade.php
@@ -72,25 +72,10 @@
 											{{Auth::user()->name}}
 										</p>
 									</li>
-									<!-- Menu Body -->
-									<li class="user-body">
-										<div class="row">
-											<div class="col-xs-4 text-center">
-												<a href="#">Followers</a>
-											</div>
-											<div class="col-xs-4 text-center">
-												<a href="#">Sales</a>
-											</div>
-											<div class="col-xs-4 text-center">
-												<a href="#">Friends</a>
-											</div>
-										</div>
-										<!-- /.row -->
-									</li>
 									<!-- Menu Footer-->
 									<li class="user-footer">
 										<div class="pull-left">
-											<a href="#" class="btn btn-default btn-flat">Profile</a>
+											<a href="#" class="btn btn-danger btn-flat"><span class = 'fa fa-sign-out'></span> Profile</a>
 										</div>
 										<div class="pull-right">
 											<a href="{{url('logout')}}" class="btn btn-default btn-flat"
