9a0ab60fb2a04046b92997d083c28d2ae0f88177
Amrani Houssain <amranidev@gmail.com>
2017-01-22 17:57:08 +0000

    create grahp view

---
+++ resources/views/graph.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Graph</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.18.0/vis.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.18.0/vis.min.js"></script>
+	<script>
+		var nodes = null;
+		var edges = null;
+		var network = null;
+		function draw() {
+		    nodes = {!! $nodes !!};
+			edges = {!! $edges !!};
+		    var container = document.getElementById('mynetwork');
+		    var data = {
+		        nodes: nodes,
+		        edges: edges
+		    };
+		    var options = {
+		        nodes: {
+		            borderWidth: 1,
+		            size: 50,
+		            color: {
+		                border: '#222222',
+		                background: '#2196F3'
+		            },
+		            font: {
+		                color: 'white'
+		            }
+		        },
+		        edges: {
+		            color: '#D50000'
+		        }
+		    };
+		    network = new vis.Network(container, data, options);
+		}
+		</script>
+
+	</script>
+
+	<style>
+		#mynetwork{
+			height: 300px;
+			border: .5px solid black;
+			border-radius: 20px;
+		}
+	</style>
+</head>
+<body onload="draw()">
+		<div class="container">
+			<h1>This is graph</h1>
+			<div id="mynetwork"></div>
+		</div>
+</body>
+</html>
