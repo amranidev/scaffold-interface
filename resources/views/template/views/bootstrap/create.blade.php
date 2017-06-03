0ee363f9e65b10e9b9a77868e2cc0935f9ddfd9a
Amrani Houssain <amranidev@users.noreply.github.com>
2017-06-03 17:35:58 +0000

    Update create.blade.php

--- resources/views/template/views/bootstrap/create.blade.php
+++ resources/views/template/views/bootstrap/create.blade.php
@@ -3,9 +3,7 @@
 @@section('content')
 <section class="content">
     <h1>Create {{$parser->singular()}}</h1>
-    <form method = 'get' action = '@{!!url("{{$parser->singular()}}")!!}'>
-        <button class = 'btn btn-danger'>{{$parser->singular()}} Index</button>
-    </form>
+        <a href="@{!!url('{{$parser->singular()}}')!!}" class = 'btn btn-danger'>{{ucwords($parser->singular())}} Index</a>
     <br>
     <form method = 'POST' action = '@{!!url("{{$parser->singular()}}")!!}'>
         <input type = 'hidden' name = '_token' value = '@{{Session::token()}}'>
@@ -25,7 +23,7 @@
             </select>
         </div>
         @endforeach
-        <button class = 'btn btn-primary' type ='submit'>Create</button>
+        <button class = 'btn btn-primary' type ='submit'><i class="fa fa-floppy-o fa-md" aria-hidden="true"></i> Create</button>
     </form>
 </section>
 @@endsection
