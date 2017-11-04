41a0581b7c724911c5d8756ddec239a604702aea
ramdani <xhijack@gmail.com>
2017-11-04 20:57:43 +0700

    fix view box

--- resources/views/template/views/bootstrap/create.blade.php
+++ resources/views/template/views/bootstrap/create.blade.php
@@ -2,42 +2,45 @@
 @@section('title','Create')
 @@section('content')
     <section class="content">
-        <div class="box-header with-border">
-            <h3 class="box-title">Create {{$parser->singular()}}</h3>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
-        <div class="box-body">
-            <a href="@{!!url('{{$parser->singular()}}')!!}" class='btn btn-danger'><i
-                        class="fa fa-home"></i> {{$parser->upperCaseFirst()}} Index</a>
-            <br>
-            <form method='POST' action='@{!!url("{{$parser->singular()}}")!!}'>
-                <input type='hidden' name='_token' value='@{{Session::token()}}'>
-                @foreach($dataSystem->dataScaffold('v') as $value)
-                    <div class="form-group">
-                        <label for="{{$value}}">{{$value}}</label>
-                        <input id="{{$value}}" name="{{$value}}" type="text" class="form-control">
+        <div class="box">
+
+            <div class="box-header with-border">
+                <h3 class="box-title">Create {{$parser->singular()}}</h3>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                @endforeach
-                @foreach($dataSystem->getForeignKeys() as $key)
-                    <div class="form-group">
-                        <label>{{$key}} Select</label>
-                        <select name='{{lcfirst(str_singular($key))}}_id' class='form-control'>
-                            @@foreach(${{str_plural($key)}} as $key => $value)
-                                <option value="@{{$key}}">@{{$value}}</option>
-                                @@endforeach
-                        </select>
-                    </div>
-                @endforeach
-                <button class='btn btn-success' type='submit'><i class="fa fa-floppy-o"></i> Save</button>
-            </form>
+                @endif
+            </div>
+            <div class="box-body">
+                <a href="@{!!url('{{$parser->singular()}}')!!}" class='btn btn-danger'><i
+                            class="fa fa-home"></i> {{$parser->upperCaseFirst()}} Index</a>
+                <br>
+                <form method='POST' action='@{!!url("{{$parser->singular()}}")!!}'>
+                    <input type='hidden' name='_token' value='@{{Session::token()}}'>
+                    @foreach($dataSystem->dataScaffold('v') as $value)
+                        <div class="form-group">
+                            <label for="{{$value}}">{{$value}}</label>
+                            <input id="{{$value}}" name="{{$value}}" type="text" class="form-control">
+                        </div>
+                    @endforeach
+                    @foreach($dataSystem->getForeignKeys() as $key)
+                        <div class="form-group">
+                            <label>{{$key}} Select</label>
+                            <select name='{{lcfirst(str_singular($key))}}_id' class='form-control'>
+                                @@foreach(${{str_plural($key)}} as $key => $value)
+                                    <option value="@{{$key}}">@{{$value}}</option>
+                                    @@endforeach
+                            </select>
+                        </div>
+                    @endforeach
+                    <button class='btn btn-success' type='submit'><i class="fa fa-floppy-o"></i> Save</button>
+                </form>
+            </div>
         </div>
     </section>
     @@endsection
--- resources/views/template/views/bootstrap/edit.blade.php
+++ resources/views/template/views/bootstrap/edit.blade.php
@@ -2,44 +2,47 @@
 @@section('title','Edit')
 @@section('content')
     <section class="content">
-        <div class="box-header with-border">
-            <h3 class="box-title">Edit {{$parser->singular()}}</h3>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
-        <div class="box-body">
-            <a href="@{!!url('{{$parser->singular()}}')!!}" class='btn btn-primary'><i
-                        class="fa fa-home"></i> {{$parser->upperCaseFirst()}} Index</a>
-            <br>
-            <form method='POST'
-                  action='@{!! url("{{$parser->singular()}}")!!}/@{!!${{$parser->singular()}}->id!!}/update'>
-                <input type='hidden' name='_token' value='@{{Session::token()}}'>
-                @foreach($dataSystem->dataScaffold('v') as $value)
-                    <div class="form-group">
-                        <label for="{{$value}}">{{$value}}</label>
-                        <input id="{{$value}}" name="{{$value}}" type="text" class="form-control"
-                               value="@{!!${{$parser->singular()}}->{{$value}}!!}">
+        <div class="box">
+
+            <div class="box-header with-border">
+                <h3 class="box-title">Edit {{$parser->singular()}}</h3>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                @endforeach
-                @foreach($dataSystem->getForeignKeys() as $key)
-                    <div class="form-group">
-                        <label>{{$key}} Select</label>
-                        <select name='{{lcfirst(str_singular($key))}}_id' class="form-control">
-                            @@foreach(${{str_plural($key)}} as $key => $value)
-                                <option value="@{{$key}}">@{{$value}}</option>
-                                @@endforeach
-                        </select>
-                    </div>
-                @endforeach
-                <button class='btn btn-success' type='submit'><i class="fa fa-floppy-o"></i> Update</button>
-            </form>
+                @endif
+            </div>
+            <div class="box-body">
+                <a href="@{!!url('{{$parser->singular()}}')!!}" class='btn btn-primary'><i
+                            class="fa fa-home"></i> {{$parser->upperCaseFirst()}} Index</a>
+                <br>
+                <form method='POST'
+                      action='@{!! url("{{$parser->singular()}}")!!}/@{!!${{$parser->singular()}}->id!!}/update'>
+                    <input type='hidden' name='_token' value='@{{Session::token()}}'>
+                    @foreach($dataSystem->dataScaffold('v') as $value)
+                        <div class="form-group">
+                            <label for="{{$value}}">{{$value}}</label>
+                            <input id="{{$value}}" name="{{$value}}" type="text" class="form-control"
+                                   value="@{!!${{$parser->singular()}}->{{$value}}!!}">
+                        </div>
+                    @endforeach
+                    @foreach($dataSystem->getForeignKeys() as $key)
+                        <div class="form-group">
+                            <label>{{$key}} Select</label>
+                            <select name='{{lcfirst(str_singular($key))}}_id' class="form-control">
+                                @@foreach(${{str_plural($key)}} as $key => $value)
+                                    <option value="@{{$key}}">@{{$value}}</option>
+                                    @@endforeach
+                            </select>
+                        </div>
+                    @endforeach
+                    <button class='btn btn-success' type='submit'><i class="fa fa-floppy-o"></i> Update</button>
+                </form>
+            </div>
         </div>
     </section>
     @@endsection
