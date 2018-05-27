558675e32fbadd8b1aa2222b52b2c594c7193d75
Amrani Houssain <houssain@airbit.com>
2018-05-27 03:11:52 +0100

    add fillable to Relation model

--- src/Models/Relation.php
+++ src/Models/Relation.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Relation extends Model
 {
-    //Relation model
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['scaffoldinterface_id', 'to', 'having'];
 }
