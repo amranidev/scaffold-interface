295741fe2ec88c06a92ddcebb644a822f376ab2b
Amrani Houssain <houssain@airbit.com>
2018-05-30 02:05:56 +0100

    Create OneToMany, ManyToMany constant

--- src/Models/Relation.php
+++ src/Models/Relation.php
@@ -7,9 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Relation extends Model
 {
     /**
+     * OneToMany Relationship.
+     */
+    const OneToMany = 'OneToMany';
+    
+    /**
+     * ManyToMany Relationship.
+     */
+    const ManyToMany = 'ManyToMany';
+
+    /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array $fillable
      */
     protected $fillable = ['scaffoldinterface_id', 'to', 'having'];
 }
