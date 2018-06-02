4f729660d83a6f4451ebe1456f145dc0164273a5
Amrani Houssain <houssain@airbit.com>
2018-06-02 06:42:20 +0100

    init DeleteCrud Event

---
+++ src/Events/DeleteCrud.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Amranidev\ScaffoldInterface\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Amranidev\ScaffoldInterface\Models\Scaffoldinterface;
+
+class DeleteCrud
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var Scaffoldinterface $scaffold.
+     */
+    public $scaffold;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Scaffoldinterface $scaffold)
+    {
+        $this->scaffold = $scaffold;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
