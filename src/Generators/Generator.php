633270706ad49eb581df4b08741363c6dedf0dfe
Amrani Houssain <houssain@airbit.com>
2018-06-03 07:05:14 +0100

    update generator

--- src/Generators/Generator.php
+++ src/Generators/Generator.php
@@ -75,10 +75,10 @@ class Generator extends Filesystem
      */
     public function views()
     {
-        $this->make($this->paths->indexPath(), $this->view->generate()['index']);
-        $this->make($this->paths->createPath(), $this->view->generate()['create']);
-        $this->make($this->paths->showPath(), $this->view->generate()['show']);
-        $this->make($this->paths->editPath(), $this->view->generate()['edit']);
+        $this->make($this->paths->indexPath(), $this->view->generate()->index);
+        $this->make($this->paths->createPath(), $this->view->generate()->create);
+        $this->make($this->paths->showPath(), $this->view->generate()->show);
+        $this->make($this->paths->editPath(), $this->view->generate()->edit);
     }
 
     /**
