7a7db1667fe2ef5af724a830f66208ac6a0c6feb
Amrani Houssain <houssain@airbit.com>
2018-07-03 22:58:27 +0100

    init ScaffoldInterfaceController

---
+++ src/Http/Controllers/ScaffoldInterfaceController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Amranidev\ScaffoldInterface\Controllers;
+
+use AppController;
+
+
+/**
+ * class ScaffoldInterfaceController
+ * 
+ * NOTE: this class is still in development Stage.
+ * 
+ * @author Houssain Amrani.
+ */
+class ScaffoldInterfaceCotroller extends AppController {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('scaffold-interface::scaffold');
+    }
+}
